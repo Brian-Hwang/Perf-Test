a1e2591244f276fb8ffb2df2fe8b47afc3d5da2c
Brian-Hwang <brian11hwangb@gmail.com>
2024-01-23 13:37:53 +0900

    combine -d and -o options

--- Memory/multi_ContentionAndUsage.py
+++ Memory/multi_ContentionAndUsage.py
@@ -3,7 +3,7 @@
 # pip3 install tqdm && sudo pip3 install psutil
 
 # Use Example: 
-# taskset -c 0 python3 multi_ContentionAndUsage.py -o 30 -s 10 -t 5 -c "0-3" -v 2 -b 1024 -n 1 -d 30 
+# taskset -c 0 python3 multi_ContentionAndUsage.py -d 30 -s 10 -t 5 -c "0-3" -v 2 -b 1024 -n 1
 
 # Caution!
 # The given taskset core for the python script will not match the core given for the "Stress" memory benchmark
@@ -62,14 +62,13 @@ def parse_arguments():
     Parses command line arguments.
     """
     parser = argparse.ArgumentParser(description='Script to perform memory stress testing and monitor memory usage.')
-    parser.add_argument('-o', '--operation_duration', type=int, default=20, help='Duration for each memory and stress test operation (in seconds). Default: 20')
+    parser.add_argument('-d', '--operation_duration', type=int, default=20, help='Duration for each memory and stress test operation (in seconds). Default: 20')
     parser.add_argument('-s', '--sleep_duration', type=int, default=5, help='Sleep duration between operations (in seconds). Default: 5')
     parser.add_argument('-t', '--total_iterations', type=int, default=10, help='Total number of iterations. Default: 10')
     parser.add_argument('-c', '--taskset_cores', default='50-53', help='CPU cores to use for the stress test (e.g., "50-53"). Default: "50-53"')
     parser.add_argument('-v', '--vm_number', type=int, default=4, help='Number of VM workers for stress test. Default: 4')
     parser.add_argument('-b', '--vm_bytes', type=int, default=256*100, help='Memory for each VM worker (in MB). Default: 25600')
     parser.add_argument('-n', '--numa', type=int, default=0, help='NUMA node and memory number. Default: 0')
-    parser.add_argument('-d', '--memory_measurement_duration', type=int, default=20, help='Memory measurement duration (in seconds). Default: 20')
     return parser.parse_args()
 
 def main():
@@ -80,7 +79,7 @@ def main():
     with tqdm(total=args.total_iterations + 1, desc="Baseline", unit="iteration") as pbar:
         # Baseline measurement without stress
         usage_data = []
-        average_memory_usage(1, args.memory_measurement_duration, usage_data)
+        average_memory_usage(1, args.operation_duration, usage_data)
         avg_memory_usage = sum(usage_data) / len(usage_data)
         memory_measurements.append(("Baseline", avg_memory_usage))
         pbar.update(1)
@@ -93,7 +92,7 @@ def main():
             stress_thread = threading.Thread(target=run_stress_command, args=(args.operation_duration, args.taskset_cores, args.vm_number, args.vm_bytes, args.numa))
             stress_thread.start()
 
-            average_memory_usage(1, args.memory_measurement_duration, usage_data)
+            average_memory_usage(1, args.operation_duration, usage_data)
 
             stress_thread.join()
 
